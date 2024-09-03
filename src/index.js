2e6cf35c358700f297b18a1e72f4aeaee716d063
Maciej Kowalski <maciej.kowalski1@proton.me>
2024-09-03 19:56:19 +0200

    Add Icons for List

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById ( 'root'));
 root.render (
