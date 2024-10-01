a3c120fc26e10315f84353ba592fcefebe2b3e41
Maciej Kowalski <maciej.kowalski1@proton.me>
2024-10-01 20:24:36 +0200

    Add ColumnForm.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,50 +1,32 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column.js';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
 import { useState } from 'react';
-//import { useEffect } from 'react';
 import shortid from 'shortid';
 
-
-
 const List = () => {
-      const [value, setValue] = useState('');
-      const [columns, setColumns] = useState([
-          { id: 1, title: 'Books', icon: 'book' },
-          { id: 2, title: 'Movies', icon: 'film' },
-          { id: 3, title: 'Games', icon: 'gamepad' }
-      ]);
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
 
-      const handleSubmit = e => {
-		e.preventDefault();
-		setColumns([...columns, { id: shortid(), title: value }]);
-		setValue('');
-      };
+	const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+	};
 
-      // useEffect(() => {
-
-      //     setTimeout(() => {
-      //     setColumns([...columns, { id: 4, title: 'Test column'}]);
-      //     }, 2000);
-
-      // }, []);
-
-      return (
-      <div className={styles.list}>
-            <header className={styles.header}>
-                  <h2 className={styles.tittle}>Thing to do<span>soon!</span></h2>
-            </header>
-            <p className={styles.description}>Interesting things I want to check out</p>
-            <section className={styles.columns}>
-                  {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
-            </section>
-            <form onSubmit={handleSubmit}>
-                  <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-                  <button>Add column</button>
-            </form>
-      </div>
-      );
-      
+	return (
+		<div className={styles.list}>
+			<header className={styles.header}>
+				<h2 className={styles.title}>Things to do<span>soon!</span></h2>
+			</header>
+			<p className={styles.description}>Interesting things I want to check out</p>
+			<section className={styles.columns}>
+				{columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+			</section>
+			<ColumnForm action={addColumn} />
+		</div>
+	);
 };
-
 
 export default List;
